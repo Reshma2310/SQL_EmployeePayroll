c1834f950dea02bc581ea534bccb5857168988b1
Reshma2310 <dreshma253@gmail.com>
2022-06-18 22:15:35 +0530

    [RESHMA] - Adding Gender Column after Name field

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -3,7 +3,7 @@ CREATE DATABASE PayRoll_Service;
 use PayRoll_Service;
 
 UC2: Creating Employee Payroll table
-Create Table Employee_PayRoll (ID INT IDENTITY(1,1) PRIMARY KEY, NAME VARCHAR(150), SALARY FLOAT, START DATE);
+Create Table Employee_PayRoll (ID INT IDENTITY(1,1) PRIMARY KEY, Name VARCHAR(150), Salary FLOAT, Start DATE);
 
 UC3: Inserting Data of Employees
 INSERT INTO Employee_PayRoll( Name, Salary, Start ) VALUES
@@ -15,5 +15,11 @@ UC4: Retrieve Data from database
 SELECT * FROM Employee_PayRoll;
 
 UC5: Retrieve Salary data of particular Employee & Employee list in given Range
-SELECT NAME, SALARY FROM employee_payroll WHERE NAME = 'Reshma';
-SELECT NAME, SALARY FROM Employee_PayRoll WHERE START BETWEEN CAST('2018-01-01' AS DATE) AND GETDATE();
+SELECT Name, Salary FROM employee_payroll WHERE Name = 'Reshma';
+SELECT Name, Salary FROM Employee_PayRoll WHERE Start BETWEEN CAST('2018-01-01' AS DATE) AND GETDATE();
+
+UC6: Add Gender to Employee Payroll Table
+ALTER TABLE Employee_PayRoll ADD GENDER VARCHAR(150);
+UPDATE Employee_Payroll set GENDER='Female' where Name = 'Reshma' or Name = 'Vahidha';
+UPDATE Employee_Payroll set GENDER='Male' where Name = 'Basith';
+SELECT ID,Name,Gender,Salary,Start FROM Employee_Payroll;
