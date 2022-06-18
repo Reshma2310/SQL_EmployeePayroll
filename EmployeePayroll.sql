d80c56aabeca578aa3d875c4d7ec63f2422979e2
Reshma2310 <dreshma253@gmail.com>
2022-06-18 20:24:22 +0530

    [RESHMA] - Create a Employee-Payroll Table

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -1,1 +1,6 @@
+UC1: Creating Database for Payroll
 CREATE DATABASE PayRoll_Service;
+use PayRoll_Service;
+
+UC2: Creating Employee Payroll table
+Create Table Employee_PayRoll (ID INT IDENTITY(1,1) PRIMARY KEY, NAME VARCHAR(150), SALARY FLOAT, START DATE)
